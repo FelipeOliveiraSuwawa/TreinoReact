57b84d6edcf73e79c78d78a90709814436aac15b
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-26 21:28:12 -0300

    d

---
+++ secretoWarudo/secretowarudo/src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+        <p>Clique Start para começar meu brother</p>
+        <button>Start</button>
+    </div>
+  )
+}
+
+export default StartScreen
