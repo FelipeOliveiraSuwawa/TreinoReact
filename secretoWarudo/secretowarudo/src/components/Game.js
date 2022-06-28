fdb6d76f0c78e08f253c431d3d2ec4a5dff315dd
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-06-27 21:40:04 -0300

    g

--- secretoWarudo/secretowarudo/src/components/Game.js
+++ secretoWarudo/secretowarudo/src/components/Game.js
@@ -3,9 +3,30 @@ import './Game.css'
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Verificar</button>
+    <div className='game'>
+      <p className="points">
+        <span>Pontuação:000</span>
+      </p>
+        <h1>Adivinhe a Palavra:</h1>
+        <h3 className='tip'>
+          Dica sobre a palavra <span>Dica...</span>
+        </h3>
+        <div className="wordContainer">
+            <span className='letter'>A</span>
+            <span className='blankSquare'></span>
+        </div>
+        <div className="letterContainer">
+          <p>Tente a adivinhar a letra da palavra:</p>
+          <form>
+            <input type="text" name='letter' maxLength="1" required />
+            <button>Jogar</button>
+          </form>
+        </div>
+        <div className="wrongLettersContainer">
+          <p>Letras ja Ultilizadas:</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
     </div>
   )
 }
