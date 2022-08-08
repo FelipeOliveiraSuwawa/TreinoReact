3ac220330fb1a97f042ddf7131ef53335a15450f
FelipeOliveiraSuwawa <felipeoliveira2002pires@gmail.com>
2022-08-08 19:03:02 -0300

    j

--- MiniBlog/miniblog/src/pages/CreatePost/CreatePost.js
+++ MiniBlog/miniblog/src/pages/CreatePost/CreatePost.js
@@ -3,49 +3,127 @@ import styles from './CreatePost.module.css'
 
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthValue } from '../../context/AuthContext'
+import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
 
 const CreatePost = () => {
-
-  const [title,setTitle] = useState("");
-  const [image,setImage] = useState("");
-  const [body,setBody] = useState("");
-  const [tags,setTags] = useState([]);
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-  }
+  const { user } = useAuthValue();
+
+  const navigate = useNavigate();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    // validate image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // check values
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    console.log(tagsArray);
+
+    console.log({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    if(formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+    navigate("/");
+  };
 
   return (
     <div className={styles.create_post}>
-        <h2>Create a Post</h2>
-        <p>Write about your interest freely and share your knowledge</p>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <span>Title:</span>
-            <input type="text" name='title' required placeholder='think a good post...' onChange={(e)=> setTitle(e.target.value)} value={title} />
-          </label>
-          <label>
-            <span>URL image:</span>
-            <input type="text" name='image' required placeholder='put a beautiful picture that 
-represents this moment' onChange={(e)=> setImage(e.target.value)} value={image} />
-          </label>
-          <label>
-            <span>Content:</span>
-            <textarea name="body" required placeholder='place the content of post' onChange={(e)=> setBody(e.target.body)} value={body}></textarea>
-          </label>
-          <label>
-            <span>Tags:</span>
-            <input type="text" name='tags' required placeholder='put your best tags' onChange={(e)=> setTags(e.target.value)} value={tags} />
-          </label>
-          <button className='btn'>Register</button>
-          {/*{!loading && <button className='btn'>Register</button>}
-          {loading && <button className='btn' disabled>Wait...</button>}
-  {error &&  <p className="error">{error}</p>} */}
-        </form>
+      <h2>Create Post</h2>
+      <p>Write about your knowedge</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="text"
+            required
+            placeholder="think a good title..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Show a image that represents this moment"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="put the post content"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Write your tags"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        {!response.loading && <button className="btn">Create Post</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Wait.. .
+          </button>
+        )}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
